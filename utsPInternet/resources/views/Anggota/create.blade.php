5aa28cc844ba32d8623870f69e00834101e9f515
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2022-05-16 15:29:10 +0700

    Membuat Tampilan Anggota

---
+++ utsPInternet/resources/views/Anggota/create.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('Jumbotron', 'Anggota')
+
+@section('content')
+
+    <div class="card d-flex p-2">
+        <form action="">
+            @csrf
+            <center>
+                <h2>Masukan Data Anggota</h2>
+            </center>
+
+            <div class="mb-3">
+                <label for="nama" class="form-label">Nama</label>
+                <input type="text" class="form-control border border-1 border-black" id="nama" placeholder="Masukan Nama">
+            </div>
+
+            <div class="mb-3">
+                <label for="no_telp" class="form-label">No Telefon</label>
+                <input type="text" class="form-control border border-1 border-black" id="no_telp" placeholder="Masukan No Telefon">
+            </div>
+
+            <div class="mb-3">
+                <label for="alamat" class="form-label">Alamat</label>
+                <textarea name="alamat" class="form-control border border-1 border-black" id="alamat" cols="30" rows="10"></textarea>
+            </div>
+
+            <button class="btn btn-info" > SIMPAN </button>
+
+        </form>
+    </div>
+
+@endsection
